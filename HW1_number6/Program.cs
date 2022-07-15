ee3dcc1a423aad9ede159810b2ff79a5379b1f4c
Zenkina <anastasiya_spirina_00@mail.ru>
2022-07-15 09:28:25 +0300

    HW1_number6

---
+++ HW1_number6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter an integer");
+int A = Convert.ToInt32(Console.ReadLine());
+int remains = A % 2;
+if (remains==0)
+{
+    Console.WriteLine(Convert.ToString(A)+ "- even number");
+}
+else
+{
+    Console.WriteLine(Convert.ToString(A)+ "- odd number");
+}
