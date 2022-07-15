01c1c274d40c32c850ce7836bfda7c9e59b4f357
Zenkina <anastasiya_spirina_00@mail.ru>
2022-07-15 09:14:13 +0300

    HW1_number4 new

--- HW2_number4/Program.cs
+++ HW2_number4/Program.cs
@@ -3,8 +3,8 @@ double A = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter the second number");
 double B = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Enter the third number");
-double С = Convert.ToDouble(Console.ReadLine());
-double max =A;
+double C = Convert.ToDouble(Console.ReadLine());
+double max = A;
 if (B>max)
 {
     max=B;
