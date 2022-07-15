842a41279b4207f2f4f3a1cdbc3d9e9b4dd0acfd
Zenkina <anastasiya_spirina_00@mail.ru>
2022-07-15 08:46:46 +0300

    task 2 is ready

--- HW1_number2/Program.cs
+++ HW1_number2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Console.WriteLine("enter the first number");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("enter the second number");
+double B = Convert.ToDouble(Console.ReadLine());
+if (A>B)
+{
+    Console.WriteLine(Convert.ToString(A) +"- max");
+}
+else
+{
+    if (A==B)
+    {
+        Console.WriteLine(Convert.ToString(A)+ "="+ Convert.ToString(B));
+    }
+    else
+    {
+       Console.WriteLine(Convert.ToString(B)+"- max") ;
+    }
+}
