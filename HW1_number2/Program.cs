88b97f172ddf5b332b031b455a1a163d2b07079d
Zenkina <anastasiya_spirina_00@mail.ru>
2022-07-15 08:55:46 +0300

    new

--- HW1_number2/Program.cs
+++ HW1_number2/Program.cs
@@ -1,10 +1,10 @@
-﻿Console.WriteLine("enter the first number");
+﻿Console.WriteLine("Enter the first number");
 double A = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("enter the second number");
+Console.WriteLine("Enter the second number");
 double B = Convert.ToDouble(Console.ReadLine());
 if (A>B)
 {
-    Console.WriteLine(Convert.ToString(A) +"- max");
+        Console.WriteLine(Convert.ToString(A) +"- max");
 }
 else
 {
