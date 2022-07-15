924795aa88a4ee72b3835986600c9cc4d24a6120
Zenkina <anastasiya_spirina_00@mail.ru>
2022-07-15 09:42:48 +0300

    HW1_number8

---
+++ HW1_number8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter a positive integer");
+int A = Convert.ToInt32(Console.ReadLine());
+int i=1;
+while (i<A)
+{
+    int remains = i % 2;
+    if (remains==0)
+    {
+         Console.Write(Convert.ToString(i)+" ");
+    }
+    i++;
+}
